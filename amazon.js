0af897f5edc85b116c83dd9f84feb66ec87a7271
Nishantm032 <134581756+Nishantm032@users.noreply.github.com>
2024-01-03 06:04:31 +0530

    Add files via upload
    Created Amazon Clone using basics of HTML CSS.

---
+++ amazon.js
@@ -0,0 +1,53 @@
+/* console.dir(document);
+console.dir(document.all);
+console.dir(document.all[134]);    // h2
+console.dir(document.all[134].innerText);
+doucment.all[134].innerText = "NEW try text";  
+*/
+
+/* document.getelementbytag("p"). inner text = whatever we want to change */
+
+/* console.dir(document.querySelector("p"));
+  console.dir(document.querySelector("#description");
+  console.dir(document.querySelector("hero_image");
+  console.dir(document.querySelector("div a");
+
+  console.dir(document.querySelectorAll("p"); */
+
+  /* let para = 'p';
+  para =  dir.selectQuery('p');
+  console.dir(para);
+
+  para.innerText;
+  para.HTMlText;
+  para.Textcontent;  */
+
+  /* classList.add()    // to add new classes
+  classList.remove() // to remove classes
+  classList.contains()  // to check if class exists   true or false
+  classList.toggle()     // to toggle between add and remove */    // to check whether the class in present if class is thier it remove or  if the class in not their then add class.
+
+  /*
+
+
+  // navigation
+
+  let p  = document.querySelector('p');
+  p.parentElement;
+
+  p.children;
+ */
+
+  // adding element
+
+  /* document.createElement('p');
+  appendChild(element)
+  append (ekement)      add element in last
+  prepend(element)      add element first 
+
+  insertAdjacentElement(where , element)    where and when to add the element */
+
+// remove ELEMENT
+/*
+removeChild(element)
+remove(element) */
